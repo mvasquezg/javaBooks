d68babe97a938648ca290b9cd9900d501a987ea3
mvasquezg <ing.mvasquezg@gmail.com>
2021-12-01 08:03:44 -0600

    Programa 03.-Mensaje en Pantalla en una sola instruccion en varias lienas del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/Welcome03.java
@@ -0,0 +1,12 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 03.- Show message in screen in one instructions with multiple line
+*/
+public class Welcome03{
+    
+    public static void main(String[] args){
+        System.out.println("Bienvenido a\nla\nprogramacion\nen Java");
+    }//end main
+
+}//end Welcome03
