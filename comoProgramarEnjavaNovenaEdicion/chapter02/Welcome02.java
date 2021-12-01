2855229b87434e3aeb45b5c7220abf4b89f59433
mvasquezg <ing.mvasquezg@gmail.com>
2021-12-01 07:51:28 -0600

    Programa 02.-Mensaje en Pantalla con varias intrucciones en una sola linea del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/Welcome02.java
@@ -0,0 +1,13 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 02.- Show message in screen with multiple instructions in one line
+*/
+public class Welcome02{
+    
+    public static void main(String[] args){
+        System.out.print("Bienvenido a ");
+        System.out.println("la programacion en Java");
+    }//end main
+
+}//end Welcome02
