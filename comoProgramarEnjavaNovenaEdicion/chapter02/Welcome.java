2dddd235d01c4b03e7eb4ba47e2b5329890ae656
mvasquezg <ing.mvasquezg@gmail.com>
2021-11-30 22:59:10 -0600

    Programa 01.-Mensaje en Pantalla del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/Welcome.java
@@ -0,0 +1,10 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 01.- Show message in screen   
+*/
+public class Welcome{
+    public static void main(String[] args){ 
+        System.out.println("Bienvenido a la programacion en Java");
+    }//end main
+}//end Welcome
