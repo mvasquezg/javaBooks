02e2d811103c0fa76561a082237fbdb61670f80e
mvasquezg <ing.mvasquezg@gmail.com>
2021-12-01 08:10:45 -0600

    Programa 04.-Mensaje en Pantalla en una sola instruccion en varias lienas y con formato del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/Welcome04.java
@@ -0,0 +1,12 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 04.- Show message in screen in one instruction with multiple line and format
+*/
+public class Welcome04{
+    
+    public static void main(String[] args){
+        System.out.printf("%s\n%s\n", "Bienvenido a ", "la programacion en Java");
+    }//end main
+
+}//end Welcome04
