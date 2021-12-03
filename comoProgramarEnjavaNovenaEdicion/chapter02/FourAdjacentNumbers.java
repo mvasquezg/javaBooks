4405c27481dd7b4f7120438908a672ffebbffb6c
mvasquezg <ing.mvasquezg@gmail.com>
2021-12-03 06:58:13 -0600

    Programa 08.-Imprime en pantalla 4 numeros adyacentes del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/FourAdjacentNumbers.java
@@ -0,0 +1,13 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 08.- Print in screen four adjacent numbers.
+*/
+
+public class FourAdjacentNumbers{
+    public static void main(String[] args){
+        System.out.print("0\n1 2\n3 4\n");
+        System.out.println("\n0\n1 2\n3 4");
+        System.out.printf("\n%d\n%d %d\n%d %d\n",0,1, 2, 3, 4);
+    }//end main
+}//end FourAdjacentNumbers
