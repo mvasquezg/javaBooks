11131c6114bed93d6b60f4ae9831437cf7568fbb
mvasquezg <ing.mvasquezg@gmail.com>
2021-12-14 22:50:47 -0600

    Programa 12.-Imprimiendo 4 figuras geometricas, el cuadrado, el circulo, el pino y el rombo del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/AsteriksFigure.java
@@ -0,0 +1,23 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 12.- Print four figures in console.
+*/
+
+
+public class AsteriksFigure{
+
+    public static void main(String[] args){
+        System.out.println("* * * * * * * * * *        * * *           *                 *"+
+        "\n*                 *      *"+"       *       * * *            *     *"+ 
+        "\n*                 *     *"+"         *    * * * * *       *           *"+
+        "\n*                 *    *"+"           *       *         *               *"+
+        "\n*                 *    *"+"           *       *       *                   *"+
+        "\n*                 *    *"+"           *       *         *               *"+
+        "\n*                 *    *"+"           *       *           *           *"+
+        "\n*                 *     *"+"         *        *             *       *"+
+        "\n*                 *      *"+"       *         *               *   *"+
+        "\n* * * * * * * * * *        * * *           *                 *");
+    }//end main
+
+}//end AsteriksFigure
