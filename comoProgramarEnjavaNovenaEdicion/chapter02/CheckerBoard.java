61bdbaae526400d2a463dadfa8e9fea43e94835d
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-21 12:48:12 -0600

    Progama 16.-Muestra en pantalla la tabla de damas del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/CheckerBoard.java
@@ -0,0 +1,12 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 16.- Show checker Board.
+*/
+public class CheckerBoard{
+
+    public static void main(String[] args){
+        System.out.println("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n");
+    }//end main
+
+}//end CheckerBoard
