6cd5328249c9e11d572eaa99fb72d1e9edaf545e
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-21 13:20:43 -0600

    Progama 16.-Muestra el area, diametro, circunferencia a partir del radio de un circulo del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/Circle.java
@@ -0,0 +1,32 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 17.- Show the area, diameter and circumference from the radius of a circle.
+*/
+import java.util.Scanner;
+
+public class Circle{
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        double radio=0, diameter=0, area=0, circumference=0;
+
+        System.out.print("Programa que calcula el diametro, el area y la circunferencia de un circulo.\nIngresa el radio del circulo: ");
+        radio=in.nextDouble();
+
+        //diameter of circle
+        diameter=2*radio;
+        System.out.println("El diametro del circulo es igual a : "+diameter);
+
+        //Area of circle
+        area=3.1416*radio*radio;
+        System.out.println("El area del circulo es igual a : "+area);
+
+        //Circumference of circle
+        circumference=3.1416*diameter;
+        System.out.println("El area del circulo es igual a : "+circumference);
+
+
+    }//end main
+
+}//end Circle
