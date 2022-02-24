be8cd3f4e65bb375768720f8effe6a6cf8a89a48
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-24 17:29:52 -0600

    Progama 01.-Se modifican las clases GeadeBook para que el metodo reciba un parametro, y la clase GradeBookApp, para llamar al metodo, pasando el parametro. Capitulo 03 del Libro Como Programar en Java Novena Edicion

--- comoProgramarEnjavaNovenaEdicion/chapter03/GradeBook.java
+++ comoProgramarEnjavaNovenaEdicion/chapter03/GradeBook.java
@@ -3,13 +3,18 @@
   *Chapter 03: Introduction to classes. objects, methods and string,
   *Program 01.- Class GradeBook for create objects this type. 
 */
-
 public class GradeBook{
 
 
-    //Method 
+    /* 
     public void showMessage(){
         System.out.println("Bienvenido al libro de calificaciones");
+    }*/
+
+    //Modified of chapter 3.3
+    //Method with parameter
+    public void showMessage(String nameCourse){
+        System.out.println("Bienvenido al libro de calificaciones: "+nameCourse);
     }//end showMessage
 
 }//end class GradeBook
--- comoProgramarEnjavaNovenaEdicion/chapter03/GradeBookApp.java
+++ comoProgramarEnjavaNovenaEdicion/chapter03/GradeBookApp.java
@@ -4,15 +4,27 @@
   *Program 01.- Class GradeBookApp is the application for execute. 
 */
 
-public class GradeBookApp{
+import java.util.Scanner;
 
-     
-    public static void main(String[] args){
+public class GradeBookApp{
+     public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+
+        String nameCourse="";
+
         //Creaate object type GradeBook
         GradeBook gradeBook01=new GradeBook();
 
-        //call method of the class GradeBook
-        gradeBook01.showMessage();
+        /*call method of the class GradeBook
+        gradeBook01.showMessage();*/
+
+        //Modified of chapter 3.3
+        System.out.print("Ingrese el nombre del curso: ");
+        nameCourse=in.nextLine();
+        System.out.println();
+
+        //call method with pased of parameter
+        gradeBook01.showMessage(nameCourse);
     }//end main
 
 }//end class GradeBookApp
