f4bb75e0675c2e53d3da6db52ee0ec1a09c0b870
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-23 19:57:28 -0600

    Progama 20.-Muestra la tabla de cuadrados y cubos de un numero del Capitulo 02 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter02/TableOfSquaresAndCubes.java
@@ -0,0 +1,26 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 02: Introduction to apps in Java,
+  *Program 20.- Show table of squares and cubes of a number.
+*/
+
+public class TableOfSquaresAndCubes{
+
+    public static void main(String[] args){
+
+        System.out.println("Numero\tCuadrado\tCubo\n"+
+                            0+"\t"+0+"\t\t"+0+"\n"+
+                            1+"\t"+1+"\t\t"+1+"\n"+
+                            2+"\t"+4+"\t\t"+8+"\n"+
+                            3+"\t"+9+"\t\t"+27+"\n"+
+                            4+"\t"+16+"\t\t"+64+"\n"+
+                            5+"\t"+25+"\t\t"+125+"\n"+
+                            6+"\t"+36+"\t\t"+216+"\n"+
+                            7+"\t"+49+"\t\t"+343+"\n"+
+                            8+"\t"+64+"\t\t"+512+"\n"+
+                            9+"\t"+81+"\t\t"+729+"\n"+
+                            10+"\t"+100+"\t\t"+1000+"\n");
+
+    }//end main
+
+}//end TableOfSquaresAndCubes
