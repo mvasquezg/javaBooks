57a8e7eadcebe289d8b6687585093c4d3bae8bcc
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-24 13:56:35 -0600

    Progama 01.-Se crean las clases GradeBook y GradeBookApp para ejecutar la aplicacion con objetos. Capitulo 03 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter03/GradeBook.java
@@ -0,0 +1,15 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 03: Introduction to classes. objects, methods and string,
+  *Program 01.- Class GradeBook for create objects this type. 
+*/
+
+public class GradeBook{
+
+
+    //Method 
+    public void showMessage(){
+        System.out.println("Bienvenido al libro de calificaciones");
+    }//end showMessage
+
+}//end class GradeBook
---
+++ comoProgramarEnjavaNovenaEdicion/chapter03/GradeBookApp.java
@@ -0,0 +1,18 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 03: Introduction to classes. objects, methods and string,
+  *Program 01.- Class GradeBookApp is the application for execute. 
+*/
+
+public class GradeBookApp{
+
+     
+    public static void main(String[] args){
+        //Creaate object type GradeBook
+        GradeBook gradeBook01=new GradeBook();
+
+        //call method of the class GradeBook
+        gradeBook01.showMessage();
+    }//end main
+
+}//end class GradeBookApp
