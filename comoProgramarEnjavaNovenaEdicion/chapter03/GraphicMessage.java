2c4d502fd4d79f15ce7db6ba91c1f9e0a0038cbd
mvasquezg <ing.mvasquezg@gmail.com>
2022-02-24 20:57:26 -0600

    Progama 03.-Muestra un mensaje en pantalla con interfaz grafica. Capitulo 03 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter03/GraphicMessage.java
@@ -0,0 +1,14 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 03: Introduction to classes. objects, methods and string,
+  *Program 03.-Show message with graphic interface. 
+*/
+import javax.swing.JOptionPane;
+
+public class GraphicMessage{
+
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Bienvenido\na\nJava");
+    }//end main
+
+}//end GraphicMessage
