ce113a9342cd3e6829f45fd730b6feda05a9aa0c
mvasquezg <ing.mvasquezg@gmail.com>
2022-03-01 04:58:43 -0600

    Progama 03.-Uso del operador incremento. Capitulo 04 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter04/Increment.java
@@ -0,0 +1,26 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 04: Control instructions part I.
+  *Program 03.-Use Increment. 
+*/
+
+public class Increment{
+
+    public static void main(String[] args){
+        int c=5;
+
+        System.out.println(c);
+        System.out.println(c++);
+        System.out.println(c);
+
+        System.out.println();
+
+        c=5;
+        System.out.println(c);
+        System.out.println(++c);
+        System.out.println(c);
+
+
+    }//end main
+
+}//end Increment
