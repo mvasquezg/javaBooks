b84414ccca1554384a60ec45a20a0d107e9133b4
mvasquezg <ing.mvasquezg@gmail.com>
2022-03-09 19:09:36 -0600

    Progama 11.-Asignacion de valores a las variables. Capitulo 04 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter04/Sum.java
@@ -0,0 +1,12 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 04: Control instructions part I.
+  *Programa 11: Asigned values to variables
+*/
+public class Sum{
+    public static void main(String[] args){
+        int sum=0, x=1;
+        sum=sum+x;
+        System.out.println("La suma es: "+sum);
+    }//end main
+}//end Sum
