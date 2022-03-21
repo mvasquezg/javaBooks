05bb7993f1bc142260fccde19d937cd119990ec9
mvasquezg <ing.mvasquezg@gmail.com>
2022-03-20 22:27:10 -0600

    Progama 25.-Muestra el valor de x e y dependiendo de la ubicacion del else. Capitulo 04 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter04/ElseLoose.java
@@ -0,0 +1,44 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 04: Control instructions part I.
+  *Programa 25: Show the values de x & y with the conditional else loose 01.
+*/
+public class ElseLoose{
+    public static void main(String[] args){
+        /*int x=9, y=11;
+
+        /*if(x<10)
+        if(y>10)
+        System.out.println("*****");
+        else
+        System.out.println("#####");
+        System.out.println("$$$$$");
+
+        if(x<10){
+            if(y>10)
+            System.out.println("*****");
+        
+        }else{
+            System.out.println("#####");
+            System.out.println("$$$$$");
+        }*/
+
+        int x=11, y=9;
+
+        /*if(x<10)
+        if(y>10)
+        System.out.println("*****");
+        else
+        System.out.println("#####");
+        System.out.println("$$$$$");*/
+
+        if(x<10){
+            if(y>10)
+            System.out.println("*****");
+        
+        }else{
+            System.out.println("#####");
+            System.out.println("$$$$$");
+        }
+    }//end main
+}//end ElseLoose
