51e64e88bcfa2d783d63847914aa992a5f828b87
mvasquezg <ing.mvasquezg@gmail.com>
2022-07-09 19:58:19 -0500

    Programa 31.-Muestra ciclo infinito de multiplo de dos usando ciclo while. Capitulo 04 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter04/InfiniteCycleOfTwo.java
@@ -0,0 +1,15 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 04: Control instructions part I.
+  *Programa 31: Infinity bucle multiple two.
+*/
+public class InfiniteCycleOfTwo{
+    public static void main(String[] args){
+        int number=2;
+
+        while(number>0){
+            System.out.print(number+" ");
+            number+=2;
+        }
+    }//end main
+}//end InfiniteCycleofTwo
