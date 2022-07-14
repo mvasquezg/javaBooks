da55bb62afb04dbbedc870aa5cbfa4587cdec323
mvasquezg <ing.mvasquezg@gmail.com>
2022-07-13 22:03:10 -0500

    Programa 01.-Muestra el conteo del 1 al 10 usando el ciclo while. Capitulo 05 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter05/CounterWhile.java
@@ -0,0 +1,18 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 05: Control instructions part II.
+  *Programa 01: Counter While.
+*/
+
+public class CounterWhile{
+    public static void main(String[] args){
+        int count=1;
+
+        while(count<=10){
+            System.out.print(count+" ");
+            count++;
+        }//end while
+        System.out.println();
+
+    }//end main
+}//end CounterWhile
