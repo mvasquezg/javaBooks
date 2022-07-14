e1a71b5651fc05757762048eae4ee31ea433513f
mvasquezg <ing.mvasquezg@gmail.com>
2022-07-13 22:19:32 -0500

    Programa 02.-Muestra el conteo del 1 al 10 usando el ciclo for. Capitulo 05 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter05/ForCounter.java
@@ -0,0 +1,13 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 05: Control instructions part II.
+  *Programa 02: Counter for.
+*/
+public class ForCounter{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }//end main
+}//end ForCounter
