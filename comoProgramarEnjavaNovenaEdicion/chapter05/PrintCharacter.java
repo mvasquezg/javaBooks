da6fb5ac28090883cbb32d2a9656f17b0b55f525
mvasquezg <ing.mvasquezg@gmail.com>
2022-08-29 16:28:50 -0500

    Programa 14.-Imprime el arroba. Capitulo 05 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter05/PrintCharacter.java
@@ -0,0 +1,17 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 05: Control instructions part II.
+  *Programa 14: Print Character At @. 
+*/
+
+public class PrintCharacter{
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print('@');
+            }
+            System.out.println();
+        }
+
+    }//end main
+}//end PrintCharacter
