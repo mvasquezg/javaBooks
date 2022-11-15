b49324a7f542a3032910b919185fcaf6c67c17bf
mvasquezg <ing.mvasquezg@gmail.com>
2022-11-15 10:20:17 -0600

    Programa 01.-Muestra el resultado de la raiz cuadrada de una expresion usando el metodo sqrt de la clase Math. Capitulo 06 del Libro Como Programar en Java Novena Edicion

---
+++ comoProgramarEnjavaNovenaEdicion/chapter06/CalculateSquareRootDefinided.java
@@ -0,0 +1,18 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 06: A more detailed analysis
+  *Programa 01: Calculate and show in screen the square root the next setences.
+    c=13.0
+    d=3.0
+    f=4.0   
+
+    (c+d*f)^1/2
+*/
+
+
+public class CalculateSquareRootDefinided{
+    public static void main(String[] args){
+        double c=13.0, d=3.0, f=4.0;
+        System.out.println(Math.sqrt(c+d*f));
+    }
+}//end class CalculateSquareRootDefinided
