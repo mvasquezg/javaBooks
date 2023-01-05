bea145224f2e0f6dc56126f1afbf8aada7edac2f
mvasquezg <ing.mvasquezg@gmail.com>
2023-01-05 13:46:10 -0600

    Programa 40.-Tabla de multiplicar agregando actulizacion del ejercicio 6.39. Capitulo 6 del Libro Como Programar en Java Novena Edicion

--- comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
+++ comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
@@ -11,11 +11,33 @@ import java.util.Scanner;
 public class MultiplicationTable{
     static int level=1;
 
-    static int multiplyTwoNumbers(int num1, int num2){
-        return num1*num2;
-    }
+    static int multiplyTwoNumbers(int num1, int num2, int operationOption){
+        int result=0;
 
-    static void guessMultiplication(){
+        switch(operationOption){
+            case 1:
+                result=num1+num2;
+                break;
+            case 2:
+                result=num1-num2;
+                break;
+            case 3:
+                result=num1*num2;
+                break;        
+            case 4:
+                result=num1/num2;
+                break;
+            case 5:
+                result=num1+num2-num1*num2/num2;
+                break;
+            default:
+                result=-1;
+                break; 
+        } 
+        return result;
+    }//end multiplyTwoNumbers
+
+    static void guessMultiplication(int operationOption){
         Scanner in=new Scanner(System.in);
         Random number1=new Random();
         Random number2=new Random();
@@ -34,7 +56,7 @@ public class MultiplicationTable{
 
         int multiplier=number1.nextInt(10)+1;
         int multiplying=number2.nextInt(10)+1;
-        int result=multiplyTwoNumbers(multiplier, multiplying);
+        int result=multiplyTwoNumbers(multiplier, multiplying, operationOption);
         
         /*Comentando el codigo del ejercicio 6.35 para agregar codigo del ejercico 6.36
         do{
@@ -93,7 +115,25 @@ public class MultiplicationTable{
 
         //Codigo 6.37
         do{
-            System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+            switch(operationOption){
+                case 1:
+                    System.out.print("Cuanto es "+multiplier+" + "+multiplying+": ");    
+                    break;
+                case 2:
+                    System.out.print("Cuanto es "+multiplier+" - "+multiplying+": ");
+                    break;
+                case 3:
+                    System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+                    break;
+                case 4:
+                    System.out.print("Cuanto es "+multiplier+" / "+multiplying+": ");
+                    break;
+                case 5:
+                    System.out.print("Cuanto es "+multiplier+" + "+multiplying+" - "+multiplier+" * "+multiplying+" / "+multiplying+" = ");
+                    break;
+                                    
+            }//end switch    
+            
             number=in.nextInt();
 
             opt1=option1.nextInt(4)+1;
@@ -118,7 +158,7 @@ public class MultiplicationTable{
                 hitCounter++;
                 multiplier=number1.nextInt(10)+1;
                 multiplying=number2.nextInt(10)+1;
-                result=multiplyTwoNumbers(multiplier, multiplying);
+                result=multiplyTwoNumbers(multiplier, multiplying, operationOption);
             }else{
                 switch(opt2){
                     case 1:
@@ -148,12 +188,13 @@ public class MultiplicationTable{
         if(prom>75){
             System.out.println("Felicidades, esta listo para pasar el siguiente nivel.\n");
             level=2;
+
         }else{
             System.out.println("Por favor pida ayuda adicional, A su instructor.\n");
         }
     }
     //Se agrego eeste codigo del ejercicio 6.38
-    static void guessMultiplicationLevelTwo(){
+    static void guessMultiplicationLevelTwo(int operationOption){
         Scanner in=new Scanner(System.in);
         Random number1=new Random();
         Random number2=new Random();
@@ -172,10 +213,28 @@ public class MultiplicationTable{
 
         int multiplier=number1.nextInt(100)-1;
         int multiplying=number2.nextInt(100)-1;
-        int result=multiplyTwoNumbers(multiplier, multiplying);
+        int result=multiplyTwoNumbers(multiplier, multiplying, operationOption);
         
         do{
-            System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+            switch(operationOption){
+                case 1:
+                    System.out.print("Cuanto es "+multiplier+" + "+multiplying+": ");    
+                    break;
+                case 2:
+                    System.out.print("Cuanto es "+multiplier+" - "+multiplying+": ");
+                    break;
+                case 3:
+                    System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+                    break;
+                case 4:
+                    System.out.print("Cuanto es "+multiplier+" / "+multiplying+": ");
+                    break;
+                case 5:
+                    System.out.print("Cuanto es "+multiplier+" + "+multiplying+" - "+multiplier+" * "+multiplying+" / "+multiplying+" = ");
+                    break;
+                                    
+            }//end switch
+            
             number=in.nextInt();
 
             opt1=option1.nextInt(4)+1;
@@ -200,7 +259,7 @@ public class MultiplicationTable{
                 hitCounter++;
                 multiplier=number1.nextInt(100)-1;
                 multiplying=number2.nextInt(100)-1;
-                result=multiplyTwoNumbers(multiplier, multiplying);
+                result=multiplyTwoNumbers(multiplier, multiplying, operationOption);
             }else{
                 switch(opt2){
                     case 1:
@@ -240,13 +299,19 @@ public class MultiplicationTable{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         int option=0;
-        
+        int operationOption=0;
 
+        //Agregando codigo del ejecrcio 6.39
+        
+        
         do{
+            System.out.print("====== Elija una opcion del menu. ======\n1.-Suma\n2.-Resta\n3.-Multiplicacion\n4.-Division\n5.-Todas la operaciones Mencionadas\nElija una opcion: ");
+            operationOption=in.nextInt();
+            
             if(level==1){
-                guessMultiplication();
+                guessMultiplication(operationOption);
             }else{
-                guessMultiplicationLevelTwo();
+                guessMultiplicationLevelTwo(operationOption);
             }
             
             System.out.print("\nNivel: "+level+"\n\nPresione -1 para salir del programa o cualquier otro numero para continuar: ");
