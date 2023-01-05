c3776f2163ffe331d84bcb730fa3edaa75100676
mvasquezg <ing.mvasquezg@gmail.com>
2023-01-05 12:34:30 -0600

    Programa 40.-Tabla de multiplicar agregando actulizacion del ejercicio 6.38. Capitulo 6 del Libro Como Programar en Java Novena Edicion

--- comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
+++ comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
 
 
 public class MultiplicationTable{
-    
+    static int level=1;
+
     static int multiplyTwoNumbers(int num1, int num2){
         return num1*num2;
     }
@@ -146,17 +147,109 @@ public class MultiplicationTable{
 
         if(prom>75){
             System.out.println("Felicidades, esta listo para pasar el siguiente nivel.\n");
+            level=2;
         }else{
             System.out.println("Por favor pida ayuda adicional, A su instructor.\n");
         }
+    }
+    //Se agrego eeste codigo del ejercicio 6.38
+    static void guessMultiplicationLevelTwo(){
+        Scanner in=new Scanner(System.in);
+        Random number1=new Random();
+        Random number2=new Random();
+
+        Random option1=new Random();
+        Random option2=new Random();
+        
+        int number=0;
+        int opt1=0;
+        int opt2=0;
+
+        int counter=0;
+        int hitCounter=0;
+        int failureCounter=0;
+        double prom=0;
+
+        int multiplier=number1.nextInt(100)-1;
+        int multiplying=number2.nextInt(100)-1;
+        int result=multiplyTwoNumbers(multiplier, multiplying);
+        
+        do{
+            System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+            number=in.nextInt();
+
+            opt1=option1.nextInt(4)+1;
+            opt2=option2.nextInt(4)+1;
+
+
+            if(number==result){
+                switch(opt1){
+                    case 1:
+                        System.out.println("Muy bien!");
+                        break;
+                    case 2:
+                        System.out.println("Excelente!");
+                        break;
+                    case 3:
+                        System.out.println("Buen trabajo!");
+                        break;
+                    case 4:
+                        System.out.println("Sigue asi!");
+                        break;            
+                }//end switch
+                hitCounter++;
+                multiplier=number1.nextInt(100)-1;
+                multiplying=number2.nextInt(100)-1;
+                result=multiplyTwoNumbers(multiplier, multiplying);
+            }else{
+                switch(opt2){
+                    case 1:
+                        System.out.println("No. Por favor intenta de nuevo.");
+                        break;
+                    case 2:
+                        System.out.println("Incorrecto. Intenta una vez mas.");
+                        break;
+                    case 3:
+                        System.out.println("No te rindas!");
+                        break;
+                    case 4:
+                        System.out.println("No Sigue intentando!");
+                        break;            
+                }//end switch
+                failureCounter++;
+            }
+            counter++;
+        }while(counter<10);
+
+
+        System.out.println("Total de ejercicios: "+counter+"\nAciertos: "+hitCounter+"\nFallados: "+failureCounter);        
+        prom=(hitCounter*100)/counter;
+
+        System.out.println("Promedio: "+prom);
+
+        if(prom>75){
+            level=1;
+            System.out.println("Felicidades, Excelente regresa a la practica en el nivel: "+level); 
+        }else{
+            System.out.println("Por favor pida ayuda adicional, A su instructor.\n");
+        }
+
+        
     }
 
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         int option=0;
+        
+
         do{
-            guessMultiplication();
-            System.out.print("\nPresione -1 para salir del programa o cualquier otro numero para continuar: ");
+            if(level==1){
+                guessMultiplication();
+            }else{
+                guessMultiplicationLevelTwo();
+            }
+            
+            System.out.print("\nNivel: "+level+"\n\nPresione -1 para salir del programa o cualquier otro numero para continuar: ");
             option=in.nextInt();
             System.out.print("\n");
 
