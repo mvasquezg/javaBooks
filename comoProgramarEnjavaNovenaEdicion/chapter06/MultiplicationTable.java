4d81c8375a5a4b78f30fae97a8fd680b70fa6d4b
mvasquezg <ing.mvasquezg@gmail.com>
2023-01-05 11:00:27 -0600

    Programa 40.-Tabla de multiplicar agregando actulizacion del ejercicio 6.37. Capitulo 6 del Libro Como Programar en Java Novena Edicion

--- comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
+++ comoProgramarEnjavaNovenaEdicion/chapter06/MultiplicationTable.java
@@ -15,23 +15,25 @@ public class MultiplicationTable{
     }
 
     static void guessMultiplication(){
+        Scanner in=new Scanner(System.in);
         Random number1=new Random();
         Random number2=new Random();
 
         Random option1=new Random();
         Random option2=new Random();
-
-
-        int multiplier=number1.nextInt(10)+1;
-        int multiplying=number2.nextInt(10)+1;
         
+        int number=0;
         int opt1=0;
         int opt2=0;
 
-        int result=multiplyTwoNumbers(multiplier, multiplying);
-        Scanner in=new Scanner(System.in);
-        int number=0;
+        int counter=0;
+        int hitCounter=0;
+        int failureCounter=0;
+        double prom=0;
 
+        int multiplier=number1.nextInt(10)+1;
+        int multiplying=number2.nextInt(10)+1;
+        int result=multiplyTwoNumbers(multiplier, multiplying);
         
         /*Comentando el codigo del ejercicio 6.35 para agregar codigo del ejercico 6.36
         do{
@@ -45,7 +47,7 @@ public class MultiplicationTable{
             }
         }while(number!=result);*/
 
-        //Codigo del ejercicio 6.36    
+        /*Codigo del ejercicio 6.36    
         do{
             System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
             number=in.nextInt();
@@ -86,7 +88,67 @@ public class MultiplicationTable{
                 }//end switch
             }
         }while(number!=result);
-                
+        */
+
+        //Codigo 6.37
+        do{
+            System.out.print("Cuanto es "+multiplier+" * "+multiplying+": ");
+            number=in.nextInt();
+
+            opt1=option1.nextInt(4)+1;
+            opt2=option2.nextInt(4)+1;
+
+
+            if(number==result){
+                switch(opt1){
+                    case 1:
+                        System.out.println("Muy bien!");
+                        break;
+                    case 2:
+                        System.out.println("Excelente!");
+                        break;
+                    case 3:
+                        System.out.println("Buen trabajo!");
+                        break;
+                    case 4:
+                        System.out.println("Sigue asi!");
+                        break;            
+                }//end switch
+                hitCounter++;
+                multiplier=number1.nextInt(10)+1;
+                multiplying=number2.nextInt(10)+1;
+                result=multiplyTwoNumbers(multiplier, multiplying);
+            }else{
+                switch(opt2){
+                    case 1:
+                        System.out.println("No. Por favor intenta de nuevo.");
+                        break;
+                    case 2:
+                        System.out.println("Incorrecto. Intenta una vez mas.");
+                        break;
+                    case 3:
+                        System.out.println("No te rindas!");
+                        break;
+                    case 4:
+                        System.out.println("No Sigue intentando!");
+                        break;            
+                }//end switch
+                failureCounter++;
+            }
+            counter++;
+        }while(counter<10);
+
+
+        System.out.println("Total de ejercicios: "+counter+"\nAciertos: "+hitCounter+"\nFallados: "+failureCounter);        
+        prom=(hitCounter*100)/counter;
+
+        System.out.println("Promedio: "+prom);
+
+        if(prom>75){
+            System.out.println("Felicidades, esta listo para pasar el siguiente nivel.\n");
+        }else{
+            System.out.println("Por favor pida ayuda adicional, A su instructor.\n");
+        }
     }
 
     public static void main(String[] args){
