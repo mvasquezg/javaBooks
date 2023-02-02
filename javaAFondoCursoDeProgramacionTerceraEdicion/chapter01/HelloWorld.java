0d7c35d37c79350179f2a1b9ddad494a5f93e96c
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-02 13:18:03 -0600

    Programa 01.-Imprimir en pantalla Hola Mundo. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+  *Book: JAVA HOW TO PROGRAM, 9TH EDITION, BY PAUL DEITEL AND HARVEY DEITEL. 
+  *Chapter 01: Introduction to programming language.
+  *Programa 01: Hello world.
+*/
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hola mundo!!!");
+    }
+}//end HelloWorld
