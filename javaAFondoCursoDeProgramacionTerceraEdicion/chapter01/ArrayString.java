3d7c28747c933d9b120df7fa1fe9e2f71d73f623
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 07:07:29 -0600

    Programa 13.-Uso de arreglos, identificando cuantos elementos tiene haciendo uso de la propiedad length. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/ArrayString.java
@@ -0,0 +1,13 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 13: Using Arrays of strings of name beatles.
+*/
+
+public class ArrayString{
+    public static void main(String[] args){
+        String[] arrayBeatles={"John", "Paul", "George", "Ringo"};
+
+        System.out.println("Los beatles son: "+arrayBeatles.length);
+    }//end main
+}//end ArrayString
