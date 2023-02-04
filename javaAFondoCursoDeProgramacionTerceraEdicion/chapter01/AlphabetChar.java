cbcc894bccf721f8fdce3fea6d7bebfa874516a9
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 09:18:36 -0600

    Programa 19.-Alphabet char. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/AlphabetChar.java
@@ -0,0 +1,15 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 19: Alphabet Char.
+*/
+
+public class AlphabetChar{
+    public static void main(String[] args){
+        for(int i='A'; i<='Z'; i++){
+            System.out.println((char) i+" ");
+
+        }//end for
+
+    }//end main
+}//end AlphabetChar
