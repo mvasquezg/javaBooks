5efa1b4e5eb453d70b0c180714c45629ba6efac3
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 09:08:41 -0600

    Programa 18.-Alphabet. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/Alphabet.java
@@ -0,0 +1,14 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 18: Alphabet.
+*/
+
+public class Alphabet{
+    public static void main(String[] args){
+        for(int i='A'; i<='Z'; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}//end Alphabet
