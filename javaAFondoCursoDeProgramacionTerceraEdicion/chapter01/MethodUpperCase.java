27f137a630a0293578c0b0516fa971e7721222c8
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 08:42:40 -0600

    Programa 16.-Uso del metodo Upper Case. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/MethodUpperCase.java
@@ -0,0 +1,12 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 16: Using the method upperCase.
+*/
+
+public class MethodUpperCase{
+    public static void main(String[] args){
+        System.out.println("Esto es una cadena. Antes de usar el metodo uppercase");
+        System.out.println("\nEsto es una cadena.".toUpperCase()+" Despues de usar el metodo uppercase\n");
+    }//end main
+}//end MethodUpperCase
