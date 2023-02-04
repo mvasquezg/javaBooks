057420328043ffa1560d45be3df26c18e49a2025
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 09:36:23 -0600

    Programa 20.-Caracter Especial. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/SpecialCharacter.java
@@ -0,0 +1,13 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 20: Special Character.
+*/
+
+public class SpecialCharacter{
+    public static void main(String[] args){
+        System.out.println("Esto \t  es un \"TABULADOR\" ");
+        System.out.println("Esto es un \nBARRA\nENE\n\n :O");
+        System.out.println("La barra es asi: \\");
+    }//end main
+}//end SpecialCharacter
