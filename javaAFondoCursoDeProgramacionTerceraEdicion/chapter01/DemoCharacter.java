f0b043a2bf31e3e3d35a431214be446019d44f57
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-04 08:59:13 -0600

    Programa 17.-Identificando como se comporta un entero con un string y un caracter. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/DemoCharacter.java
@@ -0,0 +1,24 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 17: Demo Character.
+*/
+public class DemoCharacter{
+    public static void main(String[] args){
+        
+        System.out.println("Aqui se suma un entero a una cadena: ");
+        for(int i=0; i<5; i++){
+            System.out.println(i+"A");
+        }
+
+        System.out.println();
+
+        System.out.println("Aqui se suma un entero a un caracter: ");
+        for(int i=0; i<5; i++){
+            System.out.println(i+'A');
+        }
+
+
+
+    }//end main
+}//end DemoCharacter
