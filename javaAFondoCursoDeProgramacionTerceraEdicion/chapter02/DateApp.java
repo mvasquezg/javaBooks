c0d193a3ae6230e01f9e22aa563a5c7833cad17b
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-05 14:42:17 -0600

    Programa 01.-Creando la clase FechaApp. que es la que llam al objeto Fecha pero si acceder a los campos por estar en modo private. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/DateApp.java
@@ -0,0 +1,16 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 02: Classes and Objects.
+  *Programa 01: Date
+*/
+
+public class DateApp{
+    public static void main(String[] args){
+        Date date=new Date();
+
+        date.day=2;
+        date.month=10;
+        date.year=1970;
+
+    }//end main
+}//end DateApp
