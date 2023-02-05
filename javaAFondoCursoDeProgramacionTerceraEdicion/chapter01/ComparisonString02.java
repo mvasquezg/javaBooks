752c01ce87e19a5399bf2a56b8d054e2f9296162
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-05 13:23:02 -0600

    Programa 35.-Comparando cadenas con los signos iguales, asignando el valor desde el arranque del programa. Capitulo 01 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter01/ComparisonString02.java
@@ -0,0 +1,17 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 01: Introduction to programming language.
+  *Programa 35: String comparison 02 1
+*/
+public class ComparisonString02{
+    public static void main(String[] args){
+        String string01="Hola";
+        String string02="Hola";
+        
+        if(string01==string02){
+            System.out.println("Las cadenas son iguales.");
+        }else{
+            System.out.println("Las cadenas no son iguales.");
+        }
+    }//end main
+}//end omparisonString02
