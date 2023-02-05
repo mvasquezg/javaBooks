b351f1a0c26d2f4ebdaab2178e30d679e8601660
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-05 14:35:36 -0600

    Programa 01.-Creando la clase Fecha, solo con sus atributos. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/Date.java
@@ -0,0 +1,11 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 02: Classes and Objects.
+  *Programa 01: Date
+*/
+
+public class Date{
+    private int day;
+    private int month;
+    private int year;
+}//end Date
