962a61594cf7022b1054f0d4f7310bfa93f6e526
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-05 14:51:52 -0600

    Programa 01.-Agregando metodos setter y getter a la clase Date. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

--- javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/Date.java
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/Date.java
@@ -5,7 +5,37 @@
 */
 
 public class Date{
+    
     private int day;
     private int month;
     private int year;
+
+    // ------------- Start Methods Set -------------
+    public void setDay(int day){
+        this.day=day;
+    }
+
+    public void setMonth(int month){
+        this.month=month;
+    }
+
+    public void setYear(int year){
+        this.year=year;
+    }
+    // ------------- end Methods Set -------------
+
+    // ------------- Start Methods Get -------------
+    public int getDay(){
+        return this.day;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    public void getYear(){
+        return this.year;
+    }
+    // ------------- end Methods Get -------------
+
 }//end Date
