99a8e98d0db712f8c6db11c57650b0c5afdde16f
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-08 01:07:16 -0600

    Programa 01.-Escribiendo metodos para calcular una nueva fecha a partir de dias agregados. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

--- javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/Date.java
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/Date.java
@@ -11,14 +11,28 @@ public class Date{
     private int year;
 
     //Constructor
+    public Date(){
+    }
+
+    public Date(String sDate){
+        int pos1=sDate.indexOf('/');
+        int pos2=sDate.lastIndexOf('/');
+
+        String sDay=sDate.substring(0, pos1);
+        this.day=Integer.parseInt(sDay);
+
+        String sMonth=sDate.substring(pos1+1, pos2);
+        this.month=Integer.parseInt(sMonth);
+
+        String sYear=sDate.substring(pos2+1);
+        this.year=Integer.parseInt(sYear);
+    }
+
     public Date(int day, int month, int year){
         this.day=day;
         this.month=month;
         this.year=year;
     }
-
-
-
     // ------------- Start Methods Set -------------
     public void setDay(int day){
         this.day=day;
@@ -59,6 +73,35 @@ public class Date{
         return (this.day==otherDate.day) && (this.month==otherDate.month) && (this.year==otherDate.year);
     }
 
+    private int dateTotalInDays(){
+        return year*360+month*30+day;
+    }//end dateTotalInDays
+
+    public void daysToDate(int i){
+        this.year=i/360;
+
+        int resto=i%360;
+
+        this.month=resto/30;
+
+        this.day=resto%30;
+
+        if(this.day==0){
+            this.day=30;
+            this.month--;
+        }
+
+        if(this.month==0){
+            this.month=12;
+            this.year--;
+        }
+    }//end daysToDate
+
+    public void addDays(int days){
+        int sum=dateTotalInDays()+days;
+
+        daysToDate(sum);
+    }    
     // ------------- end Others Methods -------------
 
 }//end Date
