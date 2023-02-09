d52ee39c223d662e0237b5328d54a2cd7b2b55ef
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-09 12:10:13 -0600

    Programa 02.-Escribiendo el mismos programa de Fechas, ahora en paquete y es compilado y ejecutado desde consola. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/prjDate/src/date/main/DateApp.java
@@ -0,0 +1,39 @@
+/**
+  *Book: Java in depth programming course
+  *Chapter 02: Classes and Objects.
+  *Programa 01: Date App
+
+  --------------- Comando para compilar desde la consola en paquetes ---------------
+  $> javac -sourcepath ./src/ -d ./bin/ ./src/date/main/DateApp.java
+
+  Para ejecutar desde la consola, se debe posicionar en el pathc antes del nombre del paquete
+  y desde ahi ejecutar el comando.
+  java -cp Documents/javaBooks/javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/prjDate/bin/ date.main.DateApp
+
+*/
+package date.main;
+
+import java.util.Scanner;
+import date.controller.Date;
+
+public class DateApp{
+    public static void main(String[] args){
+        
+      Scanner in=new Scanner(System.in);
+      String sDate="";
+      Date dDate;
+
+      System.out.print("Ingresa la fecha (dd/mm/aaaa): ");
+      sDate=in.next();
+      dDate=new Date(sDate);
+
+      System.out.println("La fecha ingresada es: "+dDate);
+
+      System.out.print("Ingrese numero de dias a sumar: ");
+      int numDays=in.nextInt();
+
+      dDate.addDays(numDays);
+
+      System.out.println("Sumando: "+numDays+" dias; la fecha queda: "+dDate);
+    }//end main
+}//end DateApp
