ca1e8e15aa1b5cfce5ed37016cb0d64327a2b38b
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-16 16:51:45 -0600

    Programa 04.-Variables de Instancia. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/prjInstanceVariable/src/instance/classX/X.java
@@ -0,0 +1,15 @@
+package instance.classX;
+
+public class X{
+    private int a;
+    private int b;
+
+    public X(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+
+    public String toString(){
+        return "("+a+","+b+")";
+    }
+}//end X
---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/prjInstanceVariable/src/instance/xApp/XAppMain.java
@@ -0,0 +1,13 @@
+package instance.xApp;
+
+import instance.classX.X;
+
+public class XAppMain{
+    public static void main(String[] args){
+        X x1=new X(5, 4);
+        X x2=new X(2, 7);
+
+        System.out.println("x1 = "+x1);
+        System.out.println("x2 = "+x2);
+    }//end main
+}
