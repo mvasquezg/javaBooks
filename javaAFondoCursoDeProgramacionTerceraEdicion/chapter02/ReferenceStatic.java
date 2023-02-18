0797d9567986309774fb1e72cbc71cae78554333
mvasquezg <ing.mvasquezg@gmail.com>
2023-02-17 19:05:49 -0600

    Programa 08.-Metodos estatico que no se pueden acceder. Capitulo 02 del Libro Java a Fondo. Curso de Programacion

---
+++ javaAFondoCursoDeProgramacionTerceraEdicion/chapter02/ReferenceStatic.java
@@ -0,0 +1,15 @@
+
+public final class ReferenceStatic{
+    private int a=0;
+
+    public void method(){
+        System.out.println("este es un method()");
+    }   
+    
+    public static void main(String[] args){
+        System.out.println("a vale: "+a);
+
+        method();
+    } 
+}//end ReferenceStatic
+
