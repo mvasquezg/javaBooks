faa0fc3cc1494ba7efd71c50cec13f0aa83778b0
mvasquezg <ing.mvasquezg@gmail.com>
2023-11-06 17:18:44 -0600

    Programa 001.-Mensaje en pantalla, Hola mundo.

---
+++ Java2CursoDeProgramacion4taEdicion/chapter01/HelloWorld.java
@@ -0,0 +1,9 @@
+/**Libro: Java 2 Curso de Programacion 4ta Edicion.
+    Chapter 01.-Fases en el desarrollo de un programa.
+    Programa 001.-Mensaje en pantalla.
+ */
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }
+}
