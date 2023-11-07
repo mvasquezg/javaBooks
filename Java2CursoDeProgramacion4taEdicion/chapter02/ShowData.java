5dd85e4bc8134e4a2a4848fe465044eb0a580db6
mvasquezg <ing.mvasquezg@gmail.com>
2023-11-06 18:05:47 -0600

    Programa 005.-Se muestran datos de difrentes tipos, enteros, double, String

---
+++ Java2CursoDeProgramacion4taEdicion/chapter02/ShowData.java
@@ -0,0 +1,23 @@
+/**Libro: Java 2 Curso de Programacion 4ta Edicion.
+    Chapter 02.-Introduccion a Java.
+    Programa 001.-Mostrar datos por pantalla.
+*/
+
+public class ShowData{
+    public static void main(String[] args){
+        double a=0, b=0;
+        int c=0;
+        String s;
+
+        a=3.14;
+        b=2.71;
+        c=2;
+        s="Datos";
+
+        System.out.println(s+": ");
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+    }
+
+}
