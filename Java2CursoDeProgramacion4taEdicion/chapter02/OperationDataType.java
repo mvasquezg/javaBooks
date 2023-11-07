0ba65ddf25090900b8ab593d0448d3d339dcc1ea
mvasquezg <ing.mvasquezg@gmail.com>
2023-11-06 18:27:28 -0600

    Programa 006.-Operaciones matematicas con diferente tipo de datos

---
+++ Java2CursoDeProgramacion4taEdicion/chapter02/OperationDataType.java
@@ -0,0 +1,12 @@
+public class OperationDataType{
+    public static void main(String[] args){
+        double a=10;
+        float b=20;
+        int c=2, r=0;
+
+        r=(int)(7.5*Math.sqrt(a)-b/c);
+
+        System.out.println("Resulatdo: "+r);
+
+    }
+}
